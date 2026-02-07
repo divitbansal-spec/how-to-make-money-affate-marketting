7233bb4740b8f02f020bd53d0bc91092e036a15d
divitbansal-spec <divitbansal@gmail.com>
2026-02-08 00:11:26 +0530

    Update Firebase config with provided project credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
-// Replace with your Firebase project config before deployment.
+// Project Firebase config provided by user.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAZlQE5oLcsEQysU2EcqD1oJJn3_NZ528",
+  authDomain: "course-1-66b94.firebaseapp.com",
+  projectId: "course-1-66b94",
+  storageBucket: "course-1-66b94.firebasestorage.app",
+  messagingSenderId: "1087622234661",
+  appId: "1:1087622234661:web:6ce5a69ff32be4be2e75cc",
+  measurementId: "G-QPJY3G607Q"
 };
